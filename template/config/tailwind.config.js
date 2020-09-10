5bb78221b3f4a99d4d48b4a837276fa3f2c9fb41
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-09-10 16:44:33 +0200

    Use inter font in tailwind config

--- template/config/tailwind.config.js
+++ template/config/tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     ],
   },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var'],
+      },
+    },
   },
   variants: {},
   plugins: [],
